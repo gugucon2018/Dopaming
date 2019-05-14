082463467172a7971cf3cc70b2c0f6559f01fc70
User <User@YD-PC-2-018>
2019-05-14 10:17:16 +0900

    05/14 10:17 태헌

--- dopaming/src/main/java/com/hun/www/HomeController_hun.java
+++ dopaming/src/main/java/com/hun/www/HomeController_hun.java
@@ -1,4 +1,4 @@
-package com.yedam.www;
+package com.hun.www;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -15,9 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class HomeController_hun {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(HomeController_hun.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.

892bce763c387d4b4077ccf9bdf579485c32ce2c
User <User@YD-PC-2-014>
2019-05-17 14:04:43 +0900

    05.17 2:04

--- dopaming/src/main/java/com/dopaming/www/JoonController.java
+++ dopaming/src/main/java/com/dopaming/www/JoonController.java
@@ -1,15 +1,29 @@
 package com.dopaming.www;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.dopaming.www.login.MembersVO_min;
+import com.dopaming.www.notice.NoticeService;
+import com.dopaming.www.notice.NoticeServiceImpl;
+import com.dopaming.www.notice.NoticeVO;
 
 /**
  * Handles requests for the application home page.
@@ -19,146 +33,77 @@ public class JoonController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(JoonController.class);
 	
+	NoticeService service;
+	
+	
 	//뷰연결
 	@RequestMapping(value = "/notice_insert", method = RequestMethod.GET)
 	public String notice_insert(Locale locale, Model model) {
-		//logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "joon/notice_insert_joon";
+		return "admin_tiles/admin/admin_joon/notice_insert";
 	}
 	
 	//입력값 받아오기
-	
+	@RequestMapping(value="/notice_insert", method=RequestMethod.POST)//뷰에서 notice_insert의 값이 보내어지면
+	public String notice_insert(NoticeVO vo, 
+			HttpServletRequest request,HttpSession session,
+			HttpServletResponse response) throws IOException{
+		service.notice_insert(vo);
+		if(vo == null ){
+				PrintWriter out = response.getWriter();
+				out.println("<script>");
+				out.println("alert('id error');");
+				out.println("history.go(-1);"); //이전페이지로
+				out.println("</script>");
+			return "min/adminlogin_min.empty";	
+		}
+		else {
+			session.setAttribute("notice_title",vo.getNotice_title());
+			session.setAttribute("notice_content",vo.getNotice_title());
+			return "admin_tiles/admin/admin_joon/notice_selectlist";
+		}
+	}
 	
 	@RequestMapping(value = "/notice_select", method = RequestMethod.GET)
 	public String notice_select(Locale locale, Model model) {
-		//logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "joon/notice_select_joon";
 	}
 	
 	@RequestMapping(value = "/notice_update", method = RequestMethod.GET)
 	public String notice_update(Locale locale, Model model) {
-		//logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "joon/notice_update_joon";
 	}
 	@RequestMapping(value = "/claim_insert", method = RequestMethod.GET)
 	public String claim_insert(Locale locale, Model model) {
-		//logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "joon/claim_insert_joon";
 	}
 	
 	@RequestMapping(value = "/claim_select", method = RequestMethod.GET)
 	public String claim_select(Locale locale, Model model) {
-		//logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "joon/claim_select_joon";
 	}
 	
 	@RequestMapping(value = "/notice_selectlist", method = RequestMethod.GET)
 	public String notice_selectlist(Locale locale, Model model) {
-		//logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "joon/notice_selectlist_joon";
 	}
 	
 	@RequestMapping(value = "/board_selectlist", method = RequestMethod.GET)
 	public String board_selectlist(Locale locale, Model model) {
-		//logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "joon/board_selectlist_joon";
 	}
 	
 	@RequestMapping(value = "/claim_qna_selectlist", method = RequestMethod.GET)
 	public String claim_qna_selectlist(Locale locale, Model model) {
-		//logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "joon/claim_qna_selectlist_joon";
 	}
 	
 	@RequestMapping(value = "/claim_report_selectlist", method = RequestMethod.GET)
 	public String claim_report_selectlist(Locale locale, Model model) {
-		//logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "joon/claim_report_selectlist_joon";
 	}
 	
 	@RequestMapping(value = "/claim_suggest_selectlist", method = RequestMethod.GET)
 	public String claim_suggest_selectlist(Locale locale, Model model) {
-		//logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "joon/claim_suggest_selectlist_joon";
 	}
 }
--- dopaming/src/main/webapp/WEB-INF/views/admin_tiles/admin/admin_joon/notice_insert_joon.jsp
+++ dopaming/src/main/webapp/WEB-INF/views/admin_tiles/admin/admin_joon/notice_insert_joon.jsp
@@ -6,7 +6,7 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 <script type="text/javascript">
-if("${notice.notice_no}"== -1){
+if("${notice_insert.notice_no}"== -1){
 	alert("정상적으로 공지사항이 등록 되었습니다.");
 }
 </script>
@@ -15,15 +15,15 @@ if("${notice.notice_no}"== -1){
 	<h3 align=center>공지사항-등록</h3>
 	
 	<form action="noice_insert" method="post">
-	공지제목 : <input type="text" id="notice_title" name="notice_title"   value="${notice.notice_title}" 
+	공지제목 : <input type="text" id="notice_title" name="notice_title"   value="${notice_insert.notice_title}" 
 	style="width:100%; height:40px; background-color:ivory; border:1 solid blue; 
 	font-family:굴림; font-size:10pt; color:red" onMouseOver="this.style.backgroundColor='yellow'" 
 	onMouseOut="this.style.backgroundColor='ivory'"> <br><br>
 	
-	공지내용 : <textarea id="notice_contents" name="notice_contents" 
+	공지내용 : <textarea id="notice_content" name="notice_content" 
 	style="width:100%; height:500; background-color:ivory; border:1 solid blue; 
 	font-family:굴림; font-size:10pt; color:red" onMouseOver="this.style.backgroundColor='yellow'" 
-	onMouseOut="this.style.backgroundColor='ivory'">${notice.notice_content}</textarea><br><br>
+	onMouseOut="this.style.backgroundColor='ivory'">${notice_insert.notice_content}</textarea><br><br>
 	
 	<input style="float:right;" type="submit" class="" value="등록"/>
 	</form>

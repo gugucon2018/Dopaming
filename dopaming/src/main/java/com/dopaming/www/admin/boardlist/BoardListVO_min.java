d41fbed8c8dfdb11736933365647e923b275dccf
User <User@YD-PC-2-018>
2019-05-28 18:25:36 +0900

    20190528 1825 이태헌

---
+++ dopaming/src/main/java/com/dopaming/www/admin/boardlist/BoardListVO_min.java
@@ -0,0 +1,34 @@
+package com.dopaming.www.admin.boardlist;
+
+public class BoardListVO_min {
+	private int board_no;
+	private String member_id;
+	private String board_title;
+	private float file_storage;
+	public int getBoard_no() {
+		return board_no;
+	}
+	public void setBoard_no(int board_no) {
+		this.board_no = board_no;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getBoard_title() {
+		return board_title;
+	}
+	public void setBoard_title(String board_title) {
+		this.board_title = board_title;
+	}
+	public float getFile_storage() {
+		return file_storage;
+	}
+	public void setFile_storage(float file_storage) {
+		this.file_storage = file_storage;
+	}
+	
+	
+}

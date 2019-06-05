8bc5959c8d3f7cb1b5d7a88e2371e8606933b86e
User <User@YD-PC-2-018>
2019-06-05 09:57:15 +0900

    20190605 이태헌

---
+++ dopaming/src/main/java/com/dopaming/www/admin/chart/Chartservice.java
@@ -0,0 +1,11 @@
+package com.dopaming.www.admin.chart;
+
+public interface Chartservice {
+	
+	//CRUD 기능의 메소드 구현
+	//로그인
+	ChartVO getChart(ChartVO vo) throws Exception;
+//	List<MembersVO_min> getUserList();
+//	public Integer userCount();
+//	public List<Map<String,Object>> getUserMap(MembersVO_min vo);
+}
